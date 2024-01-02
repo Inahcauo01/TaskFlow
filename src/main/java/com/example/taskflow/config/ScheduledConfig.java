3a158b37622c4cc2d598bea4c888b05b3b5bdff2
tarek <tarek.ouachani01@gmail.com>
2024-01-02 02:25:13 +0100

    update Task's status to UNDONE depending on endDateTime

--- src/main/java/com/example/taskflow/config/ScheduledConfig.java
+++ src/main/java/com/example/taskflow/config/ScheduledConfig.java
@@ -1,13 +1,19 @@
 package com.example.taskflow.config;
 
+import com.example.taskflow.domain.Task;
 import com.example.taskflow.domain.User;
+import com.example.taskflow.repository.TaskRepository;
 import com.example.taskflow.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.example.taskflow.domain.enums.TaskStatus.DONE;
+import static com.example.taskflow.domain.enums.TaskStatus.UNDONE;
 
 @Configuration
 @EnableScheduling
@@ -15,12 +21,21 @@ import java.util.List;
 public class ScheduledConfig {
 
     private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
 
     // s(0-59) min(0-59) h(0-23) j(1-31) m(1-12) j(0-7)
-    @Scheduled(cron = "0 0 0 * * ?") // excecuté chaque mi-nuit
+    @Scheduled(cron = "0 0 0 * * ?") // execute every day at midnight
     public void updateScoreJetons(){
         List<User> users = userRepository.findAll();
         users.forEach(user -> user.setJetons(2));
         userRepository.saveAll(users);
+
+        List<Task> tasks = taskRepository.findAll();
+        tasks.forEach(task -> {
+            if (task.getEndDate().isBefore(LocalDateTime.now()) && !task.getStatus().equals(DONE)){
+                task.setStatus(UNDONE);
+            }
+        });
+        taskRepository.saveAll(tasks);
     }
 }
